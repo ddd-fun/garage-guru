64a1541f88511ca26c28f988294c08cdd8cd1c07
ddd-fun <laserdev@gmail.com>
2015-03-18 07:38:08 +0100

    Added spec test.

---
+++ src/test/java/org/home/gg/domain/garage/TypeBasedVehicleSpecTest.java
@@ -0,0 +1,48 @@
+package org.home.gg.domain.garage;
+
+import org.home.gg.domain.vehicle.VehicleType;
+import org.junit.Test;
+
+import java.util.EnumSet;
+
+import static org.junit.Assert.*;
+
+public class TypeBasedVehicleSpecTest {
+
+
+
+    @Test
+    public void whenMotorbikeIsDefinedThenOnlyMotorbikeIsAccepted(){
+
+        VehicleSpec spec = new TypeBasedVehicleSpec(EnumSet.of(VehicleType.MOTORBIKE));
+
+        assertTrue(spec.isSatisfiedBy(VehicleType.MOTORBIKE));
+
+        assertFalse("other vehicle should not be accepted", spec.isSatisfiedBy(VehicleType.CAR));
+
+    }
+
+    @Test
+    public void whenCarIsDefinedThenOnlyCarIsAccepted(){
+
+        VehicleSpec spec = new TypeBasedVehicleSpec(EnumSet.of(VehicleType.CAR));
+
+        assertTrue(spec.isSatisfiedBy(VehicleType.CAR));
+
+        assertFalse("other vehicle type should not be accepted", spec.isSatisfiedBy(VehicleType.MOTORBIKE));
+    }
+
+
+
+    @Test
+    public void whenTwoTypesAreDefinedThenBothAreAccepted(){
+
+        VehicleSpec spec = new TypeBasedVehicleSpec(EnumSet.of(VehicleType.CAR,VehicleType.MOTORBIKE));
+
+        assertTrue(spec.isSatisfiedBy(VehicleType.MOTORBIKE));
+        assertTrue(spec.isSatisfiedBy(VehicleType.CAR));
+    }
+
+
+
+}
