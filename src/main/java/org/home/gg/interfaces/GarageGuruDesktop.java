40b2d049ad9d4f67879c17905fff8fcb199da6fe
volodymyr.lazoryshyn <volodymyr.lazoryshyn@wirecard.com>
2015-03-19 16:10:04 +0100

    Added application builder.

--- src/main/java/org/home/gg/interfaces/GarageGuruDesktop.java
+++ src/main/java/org/home/gg/interfaces/GarageGuruDesktop.java
@@ -9,22 +9,42 @@ import java.util.EnumSet;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class GarageGuruDesktop {
 
 
-    private static ParkingApplication application;
+    private static final Function<Garage, ParkingApplication> singleThreadedAppBuilder = ParkingApplication::new;
 
+    private static final Function<Supplier<Set<ParkingLot>>, Garage> inMemoryGarageBuilder =
+            (Supplier<Set<ParkingLot>> lotConfigurator)-> new GarageInMemoryImpl(lotConfigurator.get());
 
-    private static ParkingApplication createApplication(){
-       return null;
-    }
+    private static final Supplier<Set<ParkingLot>> twoLevelGarageConfiguration = () -> {
+
+        BiFunction<LotLocation, EnumSet, ParkingLot> lotBuilder = (LotLocation lotLocation, EnumSet supportedTypes)
+                -> new ParkingLot(lotLocation, new TypeBasedVehicleSpec(supportedTypes));
+
+        Set<ParkingLot> lotSet = new HashSet<>();
+        //first level for CAR
+        lotSet.add(lotBuilder.apply(new LotLocation("A", "1"), EnumSet.of(VehicleType.CAR)));
+        lotSet.add(lotBuilder.apply(new LotLocation("A", "2"), EnumSet.of(VehicleType.CAR)));
+        lotSet.add(lotBuilder.apply(new LotLocation("A", "3"), EnumSet.of(VehicleType.CAR)));
+        lotSet.add(lotBuilder.apply(new LotLocation("A", "4"), EnumSet.of(VehicleType.CAR)));
+        //second level for MOTORBIKE
+        lotSet.add(lotBuilder.apply(new LotLocation("B", "1"), EnumSet.of(VehicleType.MOTORBIKE)));
+        lotSet.add(lotBuilder.apply(new LotLocation("B", "2"), EnumSet.of(VehicleType.MOTORBIKE)));
+        lotSet.add(lotBuilder.apply(new LotLocation("B", "3"), EnumSet.of(VehicleType.MOTORBIKE)));
+        lotSet.add(lotBuilder.apply(new LotLocation("B", "4"), EnumSet.of(VehicleType.MOTORBIKE)));
+      return lotSet;
+    };
+
+    private static final ParkingApplication application =
+            singleThreadedAppBuilder.compose(inMemoryGarageBuilder).apply(twoLevelGarageConfiguration);
 
 
     public static void main(String[] args) {
-
-        application = createApplication();
 
         System.out.println("Enter a command: park, clean, find, free, exit");
 
